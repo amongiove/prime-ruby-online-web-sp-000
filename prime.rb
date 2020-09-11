7371a153fb91597ac9ab02dd6862cbfafdeb40ea
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-11 20:03:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(n)
-  return false if !number.integer?
+  return false if !n.integer?
 
   return false if n < 2
 
-  return true if n == 2
+  return true if n == 2 || -2
 
   (2..n-1).each do |int|
     return false if n % int == 0
